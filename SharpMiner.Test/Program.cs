6c6f47617b31e3572c63c60c88e6815b896bbfeb
agailloty <mr.clerisaxel@hotmail.fr>
2023-04-23 15:48:16 +0000

    Testing the math package

--- SharpMiner.Test/Program.cs
+++ SharpMiner.Test/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
+using System;
+
+    Func<Vector<double>, Vector<double>, Vector<double>> moyPtab = (V, poids) 
+    => (poids.PointwiseDivide(poids.Sum()) * Matrix<double>.Build.DenseOfColumnVectors(V)).ColumnSums();
+
+    var notes = new DenseVector<double> {10, 12, 14, 8, 10, 8};
+    var effectif = new DenseVector<double> {3, 10, 2, 5, 3, 1};
+
+    var res = moyPtab(notes, effectif); 
+
+    Console.WriteLine(res);
