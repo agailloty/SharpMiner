b2aef0164c625b9b049210b5f7ab63ce7c17e09d
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2023-04-24 22:22:41 +0200

    Utility class that contains various useful methods

---
+++ SharpMiner.Test/Utils.cs
@@ -0,0 +1,17 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpMiner.Test
+{
+    public static class Utils
+    {
+        public static Matrix<double> CrossProd(Matrix<double> x, Matrix<double> y)
+        {
+            return x.Transpose().Multiply(y);
+        }
+    }
+}
