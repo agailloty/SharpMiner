5ff4256715ccdb2d113f386b76723a8c50d0a0d6
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2023-04-24 22:22:20 +0200

    Reading CSV file into a matrix of double

--- SharpMiner.Test/Program.cs
+++ SharpMiner.Test/Program.cs
@@ -1,18 +1,33 @@
 ﻿using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 using SharpMiner.Test;
+using MathNet.Numerics.Data.Text;
 using System;
 
+/*
+string csvFilePath = "indicators-headless.csv";
+var data = DelimitedReader.Read<double>(csvFilePath, false, ",");
 
-var data = DenseMatrix.OfArray(new double[,]
-{
-    {1.0, 2.0, 3.0},
-    {4.0, 5.0, 6.0},
-    {7.0, 8.0, 9.0},
-    {10.0, 11.0, 12.0}
-});
+
 
 var pca = new PCA();
-var transformedData = pca.Transform(data, 2);
+var transformedData = pca.Transform(data, 10);
 
-Console.WriteLine(transformedData);
+DelimitedWriter.Write("out.csv", transformedData, ",");
+*/
+
+var m1 = DenseMatrix.OfArray(new double[,] {
+    { 1.0, 2.0, 3.0 },
+    { 4.0, 5.0, 6.0 },
+    { 7.0, 8.0, 9.0 }
+});
+
+var m2 = DenseMatrix.OfArray(new double[,] {
+    { 1.0, 2.0, 3.0, 2.4 },
+    { 4.0, 5.0, 6.0, 4.0 },
+    { 7.0, 8.0, 9.0, 5.0 }
+});
+
+var res = Utils.CrossProd(m1, m2);
+
+Console.WriteLine(res);
